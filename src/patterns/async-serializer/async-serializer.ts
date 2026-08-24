ef07cf791509d2c66e067f67e78ad41701d51230
Dermache Mohamed Djamel <dermachedjamel@gmail.com>
2026-08-24 23:27:00 +0100

    add async-serializer pattern

---
+++ src/patterns/async-serializer/async-serializer.ts
@@ -0,0 +1,49 @@
+let counter = 0;
+
+const serialize = (fn: (...args: any[]) => any) => {
+  let queue = Promise.resolve();
+
+  return (...args: any[]) => {
+    const result = queue.then(() => fn(...args));
+    queue = result.catch(() => {});
+    return result;
+  };
+};
+
+const fn = serialize(
+  (() => {
+    const cacheTime = 2000;
+    let lastRefreshed: number = 0;
+    let lastResult: unknown = undefined;
+    return async () => {
+      const currentTime = new Date().getTime();
+      // check if cache is fresh enough
+      if (lastResult === undefined || lastRefreshed + cacheTime < currentTime) {
+        // refresh the value
+        lastResult = await refresh();
+        lastRefreshed = currentTime;
+      }
+      return lastResult;
+    };
+  })(),
+);
+
+console.log(await fn());
+console.log(await fn());
+await wait();
+await wait();
+console.log(await fn());
+await wait();
+console.log(await fn());
+
+async function refresh() {
+  counter++;
+  console.log(`refreshing (${counter})`);
+  return counter;
+}
+
+function wait() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+}
